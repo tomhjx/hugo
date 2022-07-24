24aabdbce29f09d6b48553658e3489d9ca182f55
Tom <tom_hejiaxi@163.com>
2022-07-24 22:36:39 +0800

    优化tag

--- tag.sh
+++ tag.sh
@@ -10,6 +10,10 @@ fi
 
 tag=$(git describe --tags `git rev-list --tags="*" --max-count=1`)
 
+if [[ -z "${tag}" ]]; then
+    tag="0.0.0"
+fi
+
 array=(${tag//-/ })
 ver1=${array[0]}
 ver1=(${ver1//./ })
@@ -19,5 +23,6 @@ let ver1[$ver1i]+=1
 tag=$(printf ".%s" "${ver1[@]}")
 tag=${tag:1}
 echo $tag
+
 git tag $tag && git push origin $tag
 
